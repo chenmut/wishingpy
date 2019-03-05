c90d1b3f2d530792128a352f376ecd32944ef157
chenmut <tianchenlin@outlook.com>
2019-03-05 22:19:01 +0800

    函数

--- test.py
+++ test.py
@@ -57,11 +57,68 @@ print('%.2f' % 3.1415926)
 
 ## list
 
+## 条件判断
 
+age = 20
+if age >= 18:
+    print('your age is', age)
+    print('adult')
 
+## 循环
+names = ['Michael', 'Bob', 'Tracy']
+for name in names:
+    print(name)
 
+list(range(5))
+
+sum = 0
+for x in range(101):
+    sum = sum + x 
+print(sum)
     
+## 使用dict和set
+d = {'Michael':95, 'Bob':75, 'Tracy':85}
+d["Michael"]
 
+s = set([1, 2, 3, 4])
+s = set([1, 2, 3, 4, (1, 2, 3)])
+s
 
+d = {(1, ):10}
+print(d)
 
+print(str(hex(110)) + '\n' + str(hex(120)))
+
+## 定义函数
+def my_abs( x ):
+    if x >= 0:
+        return x
+    else:
+        return -x
+
+print(my_abs(-99))
+
+from abstest import my_abs
+print(my_abs(-110))
+
+def my_abs2(x):
+    if not isinstance(x, (int, float)):
+        raise TypeError('bad operand type~')
+    if x >= 0:
+        pass
+    else:
+        return
+print(my_abs2('a'))
+
+## 位置函数
+def power(x, n):
+    s = 1
+    while n > 0:
+        n = n - 1 
+        s = s * x
+    return s
+
+print(power(5, 9))
+
+# func(*args, **kw)
 
