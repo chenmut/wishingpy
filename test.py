02ea8abca4b5ccce70c7118ace5454ff4271e82b
chenmut <tianchenlin@outlook.com>
2019-03-05 10:12:08 +0800

    to list

---
+++ test.py
@@ -0,0 +1,67 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+# print("hello, world")
+# print('The quick brown fox', 'jumps over', 'the lazy dog')
+# print('100 + 200')
+
+# name = input()
+# print(name)
+
+# name = input('please enter your name: ')
+# print('hello,', name)
+
+# print('1024 * 768 =', 1024 * 768)
+
+# ------python 基础
+a = 100
+if a >= 0:
+    print(a)
+else:
+    print(-a)
+
+## 数据类型和变量
+print('I\'m  ok.')
+print("I\'m learning\nPython")
+print(r'\\\t\\')
+print('''line1 \n line2 \n line3''')
+print(r'''hello, \n
+    world''')
+True 
+False
+3 > 2
+not 1 > 2
+None
+a = 'ABC'
+b = a
+a = 'XYZ'
+print(b)
+10 / 3 
+9 / 3 
+11 // 3 
+13 % 4
+
+## 字符串和编码
+print('包含中文的str')
+ord("A")
+ord("中")
+ord("国")
+chr(25991)
+"abc".encode('ascii')
+'中文'.encode('utf-8')
+b'ABC'.decode('ascii')
+'Hello, %s' % 'world'
+print('%2d-%02d' % (300.000, 100.00))
+print('%.2f' % 3.1415926)
+'growth rate: %d%%' % 7
+
+## list
+
+
+
+
+    
+
+
+
+
